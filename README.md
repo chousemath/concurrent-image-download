b579e861cb81f9f3d5c05b9f6e6934bc9e5e5d06
Joseph Sungpil Choi <chousemath@gmail.com>
2018-08-30 22:31:44 +0900

    First

---
+++ README.md
@@ -0,0 +1,15 @@
+
+### Test script
+
+> Using main.go and `go run`
+
+```bash
+$ go run main.go 2012-hyundai-sonata-x6 https://media.alienwarearena.com/media/tux-r.jpg http://is2.4chan.org/v/1535495940072.jpg https://cdn.clien.net/web/api/file/F01/5056610/98a56e7d0a61485f8b5.JPG https://www.yyyyyyy.info/files/images/p31.jpg https://static.businessinsider.com/image/52b36ba6eab8ea58492deb16-750.jpg
+```
+
+> After using `go build` to generate the executable
+
+```bash
+$ ./concurrent-image-download 2012-hyundai-sonata-x6 https://media.alienwarearena.com/media/tux-r.jpg http://is2.4chan.org/v/1535495940072.jpg https://cdn.clien.net/web/api/file/F01/5056610/98a56e7d0a61485f8b5.JPG https://www.yyyyyyy.info/files/images/p31.jpg https://static.businessinsider.com/image/52b36ba6eab8ea58492deb16-750.jpg
+```
+
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"math/rand"
+	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+var chars = []rune("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+
+func main() {
+	fmt.Println("v0.0.1")
+	rand.Seed(time.Now().UnixNano())
+	dirName := os.Args[1]
+	newPath := filepath.Join(".", "images", dirName)
+	err := os.MkdirAll(newPath, os.ModePerm)
+	if err != nil {
+		fmt.Println("There was an error creating the directory...")
+		return
+	}
+	for _, url := range os.Args[2:] {
+		wg.Add(1)
+		go worker(&url, &dirName)
+	}
+
+	wg.Wait()
+}
+
+// worker represents each go routine
+func worker(url *string, dirName *string) {
+	fmt.Println("STARTING DOWNLOAD")
+	defer func() {
+		fmt.Println("Single image download complete!")
+		wg.Done()
+	}()
+	response, err := http.Get(*url)
+	if err != nil {
+		fmt.Println("There was an error downloading a file...")
+		return
+	}
+	defer response.Body.Close()
+	// open a file for writing
+	buf := make([]rune, 24)
+	for i := range buf {
+		buf[i] = chars[rand.Intn(len(chars))]
+	}
+	file, err := os.Create(fmt.Sprintf("./images/%s/%s.jpg", *dirName, string(buf)))
+	if err != nil {
+		fmt.Println("There was an error opening a file...")
+		return
+	}
+	defer file.Close()
+	// dump response body into a file
+	_, err = io.Copy(file, response.Body)
+	if err != nil {
+		fmt.Println("There was an error copying the file...")
+	}
+}
